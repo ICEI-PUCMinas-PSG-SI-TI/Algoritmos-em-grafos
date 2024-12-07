158d9a42404e0466cccf9e4d6df2aa31fabdac18
Andrey Felipe <andrey_felipe@icloud.com>
2024-12-07 07:44:54 -0300

    criandoprojeto

---
+++ TrabalhoGrafos/Celula.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhGrafos
+{
+    internal class Celula
+    {
+        public int no;
+        public int peso;
+        public Celula Next;
+
+        public Celula(int no, int peso)
+        {
+            this.no = no;
+            Next = null;
+            this.peso = peso;
+        }
+    }
+}
