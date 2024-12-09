2261131eda66f5115d649cdceb2788e7dfa81fc5
Andrey Felipe <andrey_felipe@icloud.com>
2024-12-08 23:12:59 -0300

    AdicionandoGrafo1500Vertices2000Arestas

--- TrabalhoGrafos/obj/Debug/net8.0/TrabalhoGrafos.AssemblyInfo.cs
+++ TrabalhoGrafos/obj/Debug/net8.0/TrabalhoGrafos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrabalhoGrafos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab831caabb9103d4567c45536cebf84399203a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789325155723624a7d09d7300f375a447c200a09")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrabalhoGrafos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrabalhoGrafos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
